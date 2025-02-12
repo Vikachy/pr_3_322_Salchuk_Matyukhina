79a7c2c4327af3d2ffcae057755e4ad46f603341
NATA <227095@edu.fa.ru>
2025-02-12 09:58:59 +0300

    Первое задание

--- DotNetDebugging/Program.cs
+++ DotNetDebugging/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNetDebugging
+int result = Fibonacci(5);
+Console.WriteLine(result);
+
+static int Fibonacci(int n)
 {
-    class Program
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
+
+    for (int i = 2; i <= n; i++)
     {
-        static void Main(string[] args)
-        {
-        }
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
     }
+
+    return n == 0 ? n1 : n2;
 }
